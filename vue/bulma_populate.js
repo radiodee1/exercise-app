e5947ea801abe349887909bd1830ede27f0b3bc3
David Liebman <radiodee1@gmail.com>
2021-02-01 19:58:49 -0500

    vis css

--- vue/bulma_populate.js
+++ vue/bulma_populate.js
@@ -40,9 +40,49 @@ tree = {
     
   };
 
+  tree2 = {
+
+    feed: 
+    [ // list of feed items...
+      {
+        show_message: true,
+        show_exercise: true,
+        show_workout: false,
+        
+        picture_large: null,
+        picture_small: null,
+
+        message: "hello-world",
+
+        messsage_obj : { //big message
+          from: "",
+          to: "",
+          message: ""
+        },
+        exercise_obj : { //exercise post
+          reps: "",
+          weight: "",
+          label: ""
+        },
+        workout_obj: { // combined exercises
+          exercise_list: [
+            { exercise_id: 0 , reps: "", weight: "", label: ""},
+            { exercise_id: 1 , reps: "", weight: "", label: ""}
+          ]
+        },
+        message_list: [ // little messages
+          { message: "one message." },
+          { message: "another message."}
+        ]
+      }
+    ]
+    
+  };
+
 
 
 data = tree;
-data.feed.push(tree.feed[0]);
+data.feed.push(tree2.feed[0]);
+data.feed[0].show_workout = true;
 
 //console.log(data.feed);
