010048b7b30b2d3044caa63ecbbaefde42de12ba
David Liebman <radiodee1@gmail.com>
2021-03-20 13:10:13 -0400

    start refactor for sql_control and sql_populate

--- server/public/javascripts/sql_populate.js
+++ server/public/javascripts/sql_populate.js
@@ -21,27 +21,8 @@ module.exports = {
     sqlUserUpdateRaw
 }
 
-/*
 
-function connection() {
-    var mysql = require('mysql');
-    console.log("here...");
-    //const config = require('../../../mysql-login.js')
-
-    //console.log(config)
-
-    var con = mysql.createConnection({
-        host: process.env.VUE_APP_MYSQL_HOST, // config.host,
-        user: process.env.VUE_APP_MYSQL_USER,  //config.user,
-        port: process.env.VUE_APP_MYSQL_PORT, //config.port,
-        password: process.env.VUE_APP_MYSQL_PASSWORD, //config.password,
-        database: "web",
-    });
-
-    return con;
-}
-*/
-
+/* ----------------------- general insert fn ------------------- */
 
 function makeInsert(table_name, columns_list, values_list, mult_rows = false) {
 
@@ -132,72 +113,13 @@ function makeSelectFormat(table_name, columns_list, where_clause = '', mult_rows
     return xx
 }
 
-/*
 
-function xquery(connection, sql, mod = modSelectFunction) {
-    var m = "";
-    var mm = "";
-    var promise = null;
-    con = connection;
-    if (!connection._connectionCalled) {
-        promise = new Promise(function (mresolve, mreject) {
-            con.query(sql, function (err, result, fields) {
-                if (err) throw err;
-                //console.log("sql run 1");
-                //console.log(" here " + fields);
-                m = mod(result);
-                //m = mod(result);
-                //console.log(m + " m " + m.length);
-                mresolve(m);
-                //if (m.length > 0) return m;
-                //return m;
-            });
-        });
-
-
-    }
-    else {
-
-        promise = new Promise(function (mresolve, mreject) {
-            con.connect(function (err) {
-                if (err) throw err;
-                //console.log("Connected!");
-
-                con.query(sql, function (err, result, fields) {
-                    if (err) throw err;
-                    //console.log("sql run 2 " + result);
-                    //console.log("x here " );
-                    m = mod(result);
-                    //console.log(m + " m " + m.length);
-                    //if (m.length > 0) return m;
-                    mresolve(m);
-
-                    //return m;
-                });
-
-            });
-
-        });
-
-    }
-    //console.log("m " + m);
-    return promise;//  promise;
-}
-
-*/
 
 function modSelectFunction(i) {
     return i;
 }
 
-/*
 
-function end(con) {
-    con.end((err) => {
-        //end connection.
-    });
-}
-*/
 
 /* ---------------- very specific fn ------------------ */
 
