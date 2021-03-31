d2ee12aea79a57b39a69f34f9b7f5f5feed3e48f
David Liebman <radiodee1@gmail.com>
2021-03-31 15:21:01 -0400

    check port then vue_app_backend_port

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ var app = express();
 
 //console.log(process.env);
 
-const assigned_port =  process.env.VUE_APP_BACKEND_PORT || process.env.PORT;
+const assigned_port = process.env.PORT || process.env.VUE_APP_BACKEND_PORT ;
 
 app.listen(assigned_port);
 
