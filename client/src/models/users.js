3ba71c82a53829ab13ce77af4228961c8c99dff8
David Liebman <radiodee1@gmail.com>
2021-03-31 13:47:54 -0400

    procfile -- user.js mentions PORT

--- client/src/models/users.js
+++ client/src/models/users.js
@@ -7,7 +7,7 @@ let axios = require("axios").default;
 
 //var items = [];
 
-const port = process.env.VUE_APP_BACKEND_PORT;
+const port = process.env.VUE_APP_BACKEND_PORT || process.env.PORT;
 const url = process.env.VUE_APP_BACKEND_URL;
 console.log(port + " >" + process.env.PORT);
 
